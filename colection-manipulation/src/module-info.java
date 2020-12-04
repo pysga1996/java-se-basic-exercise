c0ecd662f1302bfa90804b3e684b47f8e6080ca7
thanhvt <thanhvt@vissoft.vn>
2020-12-04 16:44:51 +0700

    update modules

---
+++ colection-manipulation/src/module-info.java
@@ -0,0 +1,3 @@
+open module colection.manipulation {
+    requires common.base;
+}
--- junit/test/com/codegym/ex1/TriangleClassifierTest.java
+++ junit/test/com/codegym/ex1/TriangleClassifierTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class TriangleClassifierTest {
+public class TriangleClassifierTest {
 
     @Test
     void testClassifyTriangle1() {
